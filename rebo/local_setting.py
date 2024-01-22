767d8ed687ba0d0d99827a8ba8e52fe05e52579f
saeedfadaoddini <saeedfadaoddini@gmail.com>
2024-01-22 09:45:33 +0330

    second commit

--- rebo/local_setting.py
+++ rebo/local_setting.py
@@ -1,27 +1,12 @@
 import os
-SECRET_KEY = 'django-insecure-o1qvjpyp(23yi5p($dmocx_w^#9q^_iq^@a%$t%+3cq2rnuzey'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
-ZARRINPAL_MERCHANT_ID = 'e701a7c2-73ae-11e8-b07d-005056a205be'
-DB_NAME = 'rebo2'
-DB_USER = 'rebo2'
-DB_PASS = 'rebo2'
-DB_HOST = 'localhost'
-DB_PORT = 5432
-API_MAX_SMS = 'oUSMoSz9YIujHc-XyaanUMimxpSq8rpbx3KHS1nMumA='
-API_NESHAN = 'service.ad5200afd6b144a3a99f364d1f407016'
-
-
-
-# import os
-# DEBUG = False
-# ALLOWED_HOSTS = ['*']
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-# DB_NAME = os.environ.get("DB_NAME")
-# DB_USER = os.environ.get("DB_USER")
-# DB_PASS = os.environ.get("DB_PASS")
-# DB_HOST = os.environ.get("DB_HOST")
-# DB_PORT = os.environ.get("DB_PORT")
-# API_MAX_SMS = os.environ.get("API_MAX_SMS")
-# API_NESHAN = os.environ.get("API_NESHAN")
-# ZARRINPAL_MERCHANT_ID = os.environ.get("ZARRINPAL_MERCHANT_ID")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+API_MAX_SMS = os.environ.get("API_MAX_SMS")
+API_NESHAN = os.environ.get("API_NESHAN")
+ZARRINPAL_MERCHANT_ID = os.environ.get("ZARRINPAL_MERCHANT_ID")
