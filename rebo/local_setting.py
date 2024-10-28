12cc34c82d7400aa700c123e2a85965b0193dace
fadaoddini <saeedfadaoddini@gmail.com>
2024-10-28 17:56:14 +0330

    rebo v1-0010

--- rebo/local_setting.py
+++ rebo/local_setting.py
@@ -1,6 +1,4 @@
 
-
-
 import os
 DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'rebo.ir', 'iscode.ir']
@@ -20,6 +18,7 @@ CSRF_COOKIE_SECURE = False
 CSRF_TRUSTED_ORIGINS = ['https://rebo.ir', 'https://iscode.ir']
 ADDRESS_SERVER = 'https://iscode.ir'
 BACKEND_URL = 'https://rebo.ir'
+
 
 CORS_ALLOWED_ORIGINS = [
      "https://iscode.ir",
